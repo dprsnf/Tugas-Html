c61197984208d0f6e816f38fd319f53586f37b9b
Dafanurfiat <nurfiatdaffa@gmail.com>
2024-05-21 13:14:38 +0800

    Pembaruan

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
-$host       = "localhost"; // Nama hostnya
-$username   = "root"; // Username
-$pass       = ""; // Password (Isi jika menggunakan password)
-$db         = "profile_daffa"; // Database (Isikan dengan nama database yang kamu buat)
-$conn    = mysqli_connect($host, $username, $pass, $db); // Koneksi ke MySQL
+$host       = "localhost"; 
+$username   = "root"; 
+$pass       = ""; 
+$db         = "profile_daffa"; 
+$conn    = mysqli_connect($host, $username, $pass, $db); 
 
 if (!$conn) {
     die("Not connected: " . mysqli_connect_error());
